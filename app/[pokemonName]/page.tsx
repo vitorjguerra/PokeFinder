3b28134cf18b11800dceee89d95c1b951dea084f
Vítor Johansen Guerra <vitorjguerra@hotmail.com>
2024-05-24 11:42:46 -0300

    feat/add pokemon stats

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,6 +1,7 @@
 
 import { PokemonImage } from "@/components/pokemon-images";
 import { getPokemon } from "@/lib/pokemon.api";
+import { Progress } from "@/components/ui/progress";
 
 
 export default async function PokemonPage({ params }: { params: { pokemonName: string } }) {
@@ -12,12 +13,25 @@ export default async function PokemonPage({ params }: { params: { pokemonName: s
 
     return (
         <>
-            <h1 className="text-4xl text-bold pt-4">{pokemonName}</h1>
+            <h1 className="text-4xl text-bold pt-4">{pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}</h1>
             <div className="m-4" style={{ position: "relative", width: "300px", height: "300px" }}>
                 <PokemonImage
                     image={pokemonObject.sprites.other['official-artwork'].front_default}
                     name={pokemonName}
                 />
+            </div>
+            <h3>Weight: {pokemonObject.weight}</h3>
+            <div className="flex-col">
+                {pokemonObject.stats.map((statObject: any) => {
+                    const statName = statObject.stat.name;
+                    const statValue = statObject.base_stat;
+                    return (
+                        <div className="flex items-stretch" style={{ width: "500px" }} key={statName}>
+                            <h3 className="p-3 w-2/4">{statName}: {statValue}</h3>
+                            <Progress className="w-2/4 m-auto" value={statValue} />
+                        </div>
+                    )
+                })}
             </div>
         </>
     )
